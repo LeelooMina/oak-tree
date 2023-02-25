274db900eccf758e621596fd08686a0ca2cf52a8
Crystal Campbell <blankpage@gmail.com>
2023-02-24 22:46:32 -0600

    Chore: create user & acorn seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,64 @@
 require 'faker'
 
 # Create 10 fake users
-20.times do
+10.times do
   User.create(
     username: Faker::Internet.username,
     email: Faker::Internet.email,
     password: Faker::Internet.password(min_length: 8),
-    profile_img: Faker::Avatar.image(size: "200x200"),
+    profile_img: Faker::Avatar.image(size: "200x200", set: "set4"),
     cover_img: Faker::Placeholdit.image(size: "800x400"),
     bio: Faker::Lorem.paragraph
   )
 end
 
-50.times do
+10.times do
+    User.create(
+      username: Faker::Internet.username,
+      email: Faker::Internet.email,
+      password: Faker::Internet.password(min_length: 8),
+      profile_img: Faker::Avatar.image(size: "200x200", set: "set1"),
+      cover_img: Faker::Placeholdit.image(size: "800x400"),
+      bio: Faker::Lorem.paragraph
+    )
+  end
+
+15.times do
     Acorn.create(
-      content: Faker::Lorem.sentence,
+      content:Faker::Movies::Hackers.quote,
+      date_created: Faker::Date.between(from: 1.year.ago, to: Date.today),
+      img: Faker::Placeholdit.image(size: "300x300"),
+      link: Faker::Internet.url,
+      reply: false,
+      user: User.all.sample
+    )
+  end
+
+  15.times do
+    Acorn.create(
+      content:Faker::Movies::BackToTheFuture.quote,
+      date_created: Faker::Date.between(from: 1.year.ago, to: Date.today),
+      img: Faker::Placeholdit.image(size: "300x300"),
+      link: Faker::Internet.url,
+      reply: false,
+      user: User.all.sample
+    )
+  end
+
+  15.times do
+    Acorn.create(
+      content:Faker::Movies::PrincessBride.quote,
+      date_created: Faker::Date.between(from: 1.year.ago, to: Date.today),
+      img: Faker::Placeholdit.image(size: "300x300"),
+      link: Faker::Internet.url,
+      reply: false,
+      user: User.all.sample
+    )
+  end
+
+  15.times do
+    Acorn.create(
+      content:Faker::Movies::StarWars.quote,
       date_created: Faker::Date.between(from: 1.year.ago, to: Date.today),
       img: Faker::Placeholdit.image(size: "300x300"),
       link: Faker::Internet.url,
