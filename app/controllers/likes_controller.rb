36cae66382176d24713dd2272f0d7832e209e5b6
Crystal Campbell <blankpage@gmail.com>
2023-03-05 15:54:24 -0600

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -54,10 +54,7 @@ class LikesController < ApplicationController
   def destroy
     @like.destroy
 
-    respond_to do |format|
-      format.html { redirect_to likes_url, notice: "Like was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
   end
 
   private
