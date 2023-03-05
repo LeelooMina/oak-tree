9b4d22d2a4a45e03618bc0a7bf1325bdd42d010e
Crystal Campbell <blankpage@gmail.com>
2023-03-05 15:55:48 -0600

    feat: explore page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,20 @@
 class PagesController < ApplicationController
     def index
         @acorns = Acorn.order(date_created: :desc)
-    end    
+    end 
+
+    def explore
+        @acorns = Acorn.all.sample(20)
+    end
+
+    def following
+
+        current_user.followers.each do |follower|
+         
+            follower.acorns.all.each do |acorn|
+
+
+            end
+        end
+    end
 end
